75f4d311691066247e06ff75942f24b1b6a7d7b1
FCandiotti <fernandocandiotti@xbrain.com.br>
2023-09-25 19:28:17 -0300

    Inicia Projeto

---
+++ pre-cadastro-clientes/src/test/java/br/com/desafio/precadastroclientes/PreCadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.precadastroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreCadastroClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
